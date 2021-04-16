d0fdb0cd82c448728455e5b86cac93212b3705f5
gpontesss <pontes.guisilva@gmail.com>
2021-04-16 01:44:59 -0300

    Add an alternative binding to resize window

--- config.h
+++ config.h
@@ -123,6 +123,7 @@ static Button buttons[] = {
 	{ ClkClientWin,         MODKEY,           Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,           Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,           Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY|ShiftMask, Button1,        resizemouse,    {0} },
 	{ ClkTagBar,            0,                Button1,        view,           {0} },
 	{ ClkTagBar,            0,                Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,           Button1,        tag,            {0} },
